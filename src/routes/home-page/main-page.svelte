bede9745ee3ee8900a3ca37b21d5b470b876c99f
Elazar <elazrosenfeld@gmail.com>
2023-09-07 17:54:05 -0400

    a little fix

--- src/routes/home-page/main-page.svelte
+++ src/routes/home-page/main-page.svelte
@@ -1,32 +1,40 @@
-<div class="outer-box" >
+<div class="outer-box">
     <h1 class="title">HI I AM ELAZAR</h1>
     <h3 class="title">I AM A WEB DEVELOPER IN OHIO</h3>
     <h3 class="title">I LIKE TO KEEP THINGS SIMPLE</h3>
-    <img class="pic" src="img/Bak (36) (1).JPEG" alt="">
+
+    <img class="pic" src="img/Bak (36) (1).JPEG" alt="" />
+    <br />
+    <p class="about">I have completed a course from pcs lakewood new jersey</p>
+    <p class="about">for software development and I am currently interning</p>
+    <p class="about">and working with the framework of svelte</p>
 </div>
 
-
 <style>
-   :global(body) {
-        background-color: grey
-        
+    :global(body) {
+        background-color: grey;
     }
     .outer-box {
-        height: 10000px;      
+        height: 10000px;
     }
-   .title{
-       z-index: 10;
+    .title {
+        z-index: 10;
         position: relative;
-        left: 38%;
-        top: 4%;
+        left: 48%;
+        top: 2.5%;
         color: white;
     }
-    .pic{
+    .about{
         position: relative;
-        top: 1.5%;
+        top: -2.5%;
+        left: 40%;
+        color: rgb(63, 15, 15);
+    }
+    .pic {
+        position: relative;
+        top: .5%;
         width: 400px;
-        left: 55%;
+        left: 65%;
         background-color: white;
-        
     }
 </style>
