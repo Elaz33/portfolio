fb846904776767bd03a0cb894426a477c44aefca
Elazar <elazrosenfeld@gmail.com>
2023-09-14 15:47:14 -0400

    updates

--- src/routes/home-page/main-page.svelte
+++ src/routes/home-page/main-page.svelte
@@ -1,15 +1,58 @@
 <div class="outer-box">
     <h1 class="title">HI! I AM ELAZAR! I AM A WEB DEVELOPER</h1>
     <h1 class="title">IN OHIO.</h1>
-    <h3 class="title">I LIKE TO KEEP THINGS SIMPLE.</h3>
-
+    <h3 class="title">MY MOTTO IS NEVER EVER COMPLICATE THINGS.</h3>
+  
     <img class="pic" src="img/Bak (36) (1).JPEG" alt="" />
     <br class="line" />
     <p class="about">
         I have completed a course from PCS in Lakewood New Jersey
     </p>
+    <p />
     <p class="about">for Software Development and I am currently interning</p>
     <p class="about">and working within the framework of Svelte</p>
+
+    <img
+        class="icon"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original-wordmark.svg"
+    />
+    <img
+        class="icon"
+        src="img/javascript-39402.png"
+    />
+
+    <img
+        class="icon"
+        src="img/imageedit_5_8327855680.png"
+    />
+
+    <img
+        class="icon"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg"
+    />
+
+    <img
+        class="icon"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+    />
+
+    <img
+        class="icon"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+    />
+
+    <img
+        class="icon"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
+    />
+
+    <img
+        class="icon"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
+    />
+
+    <!-- <i class="devicon-github-original-wordmark"></i> -->
+  
 </div>
 
 <style>
@@ -23,17 +66,17 @@
     .title {
         z-index: 10;
         position: relative;
-        left: 20%;
+        left: 16%;
         top: 2.5%;
         font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
-        color: rgb(83, 59, 59);
+        color: rgb(70, 134, 134);
     }
     .about {
         position: relative;
         top: -2.5%;
-        left: 20%;
+        left: 16%;
         font-size: larger;
-        color: rgb(135, 117, 117);
+        color: rgb(43, 79, 43);
         font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
     }
 
@@ -45,7 +88,51 @@
         border-radius: 8px;
         background-color: rgb(135, 117, 117);
     }
+    .icon {
+        position: relative;
+        width: 60px;
+        left: 16%;
+        top: -1%;
+        
+    }
     .line {
         position: relative;
+    }
+
+    @media (max-width: 670px) {
+        .pic {
+            right: 0;
+        }
+        .outer-box {
+            overflow: visible;
+        }
+        .title {
+            top: 5.5%;
+            color: rgb(70, 134, 134);
+        }
+        .about {
+            top: 0.5%;
+        }
+        .icon{
+            top: 0.7%;
+        }
+    }
+
+      @media (max-width: 400px) {
+        .pic {
+            left: 5%;
+        }
+       
+        .title {
+            top: 5.5%;
+            color: rgb(70, 134, 134);
+        }
+        .about {
+            top: 0.5%;
+        }
+        .icon{
+            top: 0.7%;
+            width: 38px;
+        }
     }
 </style>
--- src/routes/home-page/nav-bar.svelte
+++ src/routes/home-page/nav-bar.svelte
@@ -1,7 +1,9 @@
 <div class="header">
     <div class="nav-bar">
-        
-         <div     class="header-block">CAR PROJECT </div><div     class="header-block">PROJECT B </div><div     class="header-block">PROJECT C  </div><div     class="header-block"> PROJECT D</div> 
+        <div class="header-block">CAR PROJECT</div>
+        <div class="header-block">PROJECT B</div>
+        <div class="header-block">PROJECT C</div>
+        <div class="header-block">PROJECT D</div>
     </div>
     <div class="name-block">
         <pre class="my-name">ELAZAR ROSENFELD</pre>
@@ -18,9 +20,8 @@
         display: flex;
         height: 50px;
         width: 400px;
-       
         left: 50%;
-        justify-content: space-evenly;
+        /* justify-content: space-evenly; */
         gap: 30px;
         /* border: solid red; */
         color: white;
@@ -33,7 +34,8 @@
         left: 20%;
         top: -60px;
         font-size: xx-large;
-        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+            "Lucida Sans", Arial, sans-serif;
 
         color: white;
         /* border: solid green; */
@@ -44,10 +46,55 @@
     }
     .header-block {
         position: relative;
-       
-        
-        top: 10px;
+        top: 25px;
         font-size: small;
-         font-family: Georgia, 'Times New Roman', Times, serif;
+        font-family: Georgia, "Times New Roman", Times, serif;
+    }
+    @media (min-width: 950px) {
+        .nav-bar {
+            left: 50%;
+        }
+        .name-block {
+            left: 20%;
+        }
+    }
+
+    @media (max-width: 900px) {
+        .nav-bar {
+            left: 50%;
+        }
+        .name-block {
+            left: 15%;
+        }
+    }
+
+    @media (max-width: 750px) {
+        .nav-bar {
+            left: 55%;
+            top: 50px;
+        }
+        .name-block {
+            left: 17%;
+        }
+    }
+
+    @media (max-width: 750px) {
+        .nav-bar {
+            left: 55%;
+            top: 50px;
+        }
+        .name-block {
+            left: 17%;
+        }
+    }
+
+    @media (max-width: 400px) {
+        .nav-bar {
+            left: 8%;
+            top: 40px;
+        }
+        .name-block {
+            left: 30%;
+        }
     }
 </style>
