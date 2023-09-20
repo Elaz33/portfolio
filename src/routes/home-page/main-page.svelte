d9ae622d2c439ab1ee98fee4874a2c0b1f06fe5e
Elazar <elazrosenfeld@gmail.com>
2023-09-20 14:22:40 -0400

    more fixes

--- src/routes/home-page/main-page.svelte
+++ src/routes/home-page/main-page.svelte
@@ -1,8 +1,11 @@
 <div class="outer-box">
-    <h1 class="title">HI! I AM ELAZAR! I AM A WEB DEVELOPER</h1>
-    <h1 class="title">IN OHIO.</h1>
-    <h3 class="title">MY MOTTO IS NEVER EVER COMPLICATE THINGS.</h3>
-  
+    <h1 class="title">HI I AM ELAZAR</h1>
+    <h1 class="title">I AM A WEB DEVELOPER</h1>
+    <h1 class="title">IN OHIO</h1>
+    <h3 class="title">
+        The definition of genius is taking the complex and making it simple
+    </h3>
+
     <img class="pic" src="img/Bak (36) (1).JPEG" alt="" />
     <br class="line" />
     <p class="about">
@@ -11,53 +14,46 @@
     <p />
     <p class="about">for Software Development and I am currently interning</p>
     <p class="about">and working within the framework of Svelte</p>
+    <div class="icon-row">
+        <img
+            class="icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original-wordmark.svg"
+        />
+        <img class="icon" src="img/javascript-39402.png" />
 
-    <img
-        class="icon"
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original-wordmark.svg"
-    />
-    <img
-        class="icon"
-        src="img/javascript-39402.png"
-    />
+        <img class="icon" src="img/imageedit_5_8327855680.png" />
 
-    <img
-        class="icon"
-        src="img/imageedit_5_8327855680.png"
-    />
+        <img
+            class="icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg"
+        />
 
-    <img
-        class="icon"
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg"
-    />
+        <img
+            class="icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+        />
 
-    <img
-        class="icon"
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-    />
+        <img
+            class="icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+        />
 
-    <img
-        class="icon"
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
-    />
+        <img
+            class="icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
+        />
 
-    <img
-        class="icon"
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
-    />
-
-    <img
-        class="icon"
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
-    />
-
+        <img
+            class="icon git"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
+        />
+    </div>
     <!-- <i class="devicon-github-original-wordmark"></i> -->
-  
 </div>
 
 <style>
     :global(body) {
-        background-color: rgb(14, 12, 12);
+        background-color: rgb(75, 66, 66);
     }
     .outer-box {
         height: 10000px;
@@ -66,7 +62,7 @@
     .title {
         z-index: 10;
         position: relative;
-        left: 16%;
+        left: 20%;
         top: 2.5%;
         font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
         color: rgb(70, 134, 134);
@@ -74,7 +70,7 @@
     .about {
         position: relative;
         top: -2.5%;
-        left: 16%;
+        left: 20%;
         font-size: larger;
         color: rgb(43, 79, 43);
         font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
@@ -84,22 +80,31 @@
         position: relative;
         top: 0.5%;
         width: 400px;
-        left: 44%;
+        left: 60%;
         border-radius: 8px;
         background-color: rgb(135, 117, 117);
     }
-    .icon {
+    .icon-row {
+        display: flex;
         position: relative;
+        top: -1.4%;
+        left: 18%;
+        flex-direction: row;
+        gap: 20px;
+    }
+    .icon {
         width: 60px;
         left: 16%;
         top: -1%;
-        
+    }
+    .git{
+      
     }
     .line {
         position: relative;
     }
 
-    @media (max-width: 670px) {
+    @media (max-width: 1525px) {
         .pic {
             right: 0;
         }
@@ -108,29 +113,35 @@
         }
         .title {
             top: 5.5%;
-            color: rgb(70, 134, 134);
+            color: whitesmoke;
         }
         .about {
-            top: 0.5%;
+            top: 0.6%;
         }
-        .icon{
+        .icon-row {
+            top: 0.9%;
+        }
+        .icon {
             top: 0.7%;
         }
     }
 
-      @media (max-width: 400px) {
+    @media (max-width: 400px) {
         .pic {
             left: 5%;
         }
-       
+
         .title {
             top: 5.5%;
-            color: rgb(70, 134, 134);
+            color: whitesmoke;
         }
         .about {
             top: 0.5%;
         }
-        .icon{
+        .icon-row {
+            top: 0.8%;
+        }
+        .icon {
             top: 0.7%;
             width: 38px;
         }
